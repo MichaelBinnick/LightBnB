0729c5b76694e731feb576084f61195d229376be
Michael Binnick <mbinnick@live.ca>
2022-09-22 17:27:04 +0000

    refactors getAllReservations function to use dynamic query

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -21,10 +21,11 @@ const pool = new Pool({
 const getUserWithEmail = function(email) {
   return pool
     .query(`
-    SELECT *
-    FROM users 
-    WHERE email = $1;
-    `, [email])
+      SELECT *
+      FROM users 
+      WHERE email = $1;
+      `, [email]
+    )
     .then((result) => {
       if (result.rows[0]) {
         return Promise.resolve(result.rows[0]);
@@ -46,10 +47,11 @@ exports.getUserWithEmail = getUserWithEmail;
 const getUserWithId = function(id) {
   return pool
     .query(`
-    SELECT *
-    FROM users 
-    WHERE id = $1;
-    `, [id])
+      SELECT *
+      FROM users 
+      WHERE id = $1;
+      `, [id]
+    )
     .then((result) => {
       if (result.rows[0]) {
         return Promise.resolve(result.rows[0]);
@@ -72,10 +74,11 @@ exports.getUserWithId = getUserWithId;
 const addUser =  function(user) {
   return pool
     .query(`
-    INSERT INTO users (name, email, password)
+      INSERT INTO users (name, email, password)
       VALUES ($1, $2, $3)
-    RETURNING *;
-    `, [user.name, user.email, user.password])
+      RETURNING *;
+      `, [user.name, user.email, user.password]
+    )
     .then((result) => {
       if (result.rows[0]) {
         return Promise.resolve(result.rows[0]);
@@ -97,7 +100,31 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  
+  return pool
+    .query(`
+      SELECT reservations.id, properties.title, properties.cost_per_night, reservations.start_date, avg(rating) as average_rating
+      FROM reservations
+      JOIN properties ON reservations.property_id = properties.id
+      JOIN property_reviews ON properties.id = property_reviews.property_id
+      WHERE reservations.guest_id = $1
+      GROUP BY properties.id, reservations.id
+      ORDER BY reservations.start_date
+      LIMIT $2;
+      `, [guest_id, limit]
+    )
+    .then((result) => {
+      if (result.rows) {
+        return Promise.resolve(result.rows);
+      } else {
+        return null;
+      }
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+
+  // return getAllProperties(null, 2);
 }
 exports.getAllReservations = getAllReservations;
 
