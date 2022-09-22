4aeb3820acdca97d8085309739ed638fe04092ef
Michael Binnick <mbinnick@live.ca>
2022-09-22 19:27:01 +0000

    refactors getAllProperties query to take user options for filtering results

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -137,8 +137,61 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
+  
+  //updated
+  const queryParams = [];
+
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+  }
+
+  if (options.owner_id) {
+    queryParams.push(`${options.user_id}`);
+    if (queryParams.length > 0) {
+      queryString += ` AND `;
+    } else {
+      queryString += ` WHERE `;
+    }
+    queryString += `owner_id = $${queryParams.length}`;
+  }
+
+  if (options.minimum_price_per_night) {
+    queryParams.push(Number(`${options.minimum_price_per_night}`));
+    if (queryParams.length > 0) {
+      queryString += ` AND `;
+    } else {
+      queryString += ` WHERE `;
+    }
+    queryString += `cost_per_night >= $${queryParams.length}`;
+  }
+  if (options.maximum_price_per_night) {
+    queryParams.push(Number(`${options.maximum_price_per_night}`));
+    if (queryParams.length > 0) {
+      queryString += ` AND `;
+    } else {
+      queryString += ` WHERE `;
+    }
+    queryString += `cost_per_night <= $${queryParams.length}`;
+  }
+
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  console.log(queryString, queryParams);
+
   return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
+    .query(queryString, queryParams)
     .then((result) => {
       return result.rows;
     })
